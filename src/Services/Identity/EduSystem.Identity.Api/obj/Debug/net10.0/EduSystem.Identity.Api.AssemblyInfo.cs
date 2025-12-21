2121c5614475e55584bda636448a779b22247977
Mithun Chandra Howlader <mithun@primetechbd.com>
2025-12-21 10:21:20 +0600

    commit for refactor authmanager

--- src/Services/Identity/EduSystem.Identity.Api/obj/Debug/net10.0/EduSystem.Identity.Api.AssemblyInfo.cs
+++ src/Services/Identity/EduSystem.Identity.Api/obj/Debug/net10.0/EduSystem.Identity.Api.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EduSystem.Identity.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c1de4b2447715f3084aa6b3546a18fe31a1495f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4b52b6796e9dc88471c45c825361ccc15e35b1fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("EduSystem.Identity.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EduSystem.Identity.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
